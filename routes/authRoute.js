0d79d9ced464de0283ce6bda978ccc92f7720b02
morsch28 <smors289@gmail.com>
2025-07-08 12:35:57 +0300

    responsive

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -36,7 +36,7 @@ router.post("/sign-in", async (req, res) => {
       process.env.JWT_SECRET
     );
 
-    res.status(200).send(token);
+    res.status(200).json(token);
   } catch (error) {
     res.status(500).send(error.message);
   }
